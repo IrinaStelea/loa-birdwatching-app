17a1e6404771e8eb374197c0f61cac9c201bb92a
Irina Stelea <capatul.lumii@gmail.com>
2022-09-16 16:46:10 +0200

    small fix for heroku cors https conflict

--- server/index.js
+++ server/index.js
@@ -219,7 +219,7 @@ app.get("/logout", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 //the index.html will be in the build folder after compiling
-app.get("*", (req, res) => {
+app.get("*", cors, (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/public", "index.html"));
 });
 
